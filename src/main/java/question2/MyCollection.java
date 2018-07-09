df312c49d8e4b5245d6ce6144474b8a37118013e
punitnagar5 <40259421+punitnagar5@users.noreply.github.com>
2018-07-09 14:16:42 +0530

    Add files via upload

--- src/main/java/question2/MyCollection.java
+++ src/main/java/question2/MyCollection.java
@@ -1,52 +1,117 @@
-package question2;
+package question1;
 
 /**
- * Please implement Dynamic Array Interface
- * 
- * @author
+ * Please implement Dynamic Array Interface 
+ * @Punit Nagar 
  *
  */
-public class MyCollection implements DynamicArray {
-	// Uncomment commented section
+public class MyCollection implements DynamicArray{
+// Uncomment commented section
+    
+    /**
+     * You can declare local/global variables as per your requirement
+     */
+    @SuppressWarnings("unused")
+    private int[] numArray;
+    /**
+     * It constructs an empty Collection object with an array capacity specified by the integer
+        parameter "arraySize".
+     */
+    public MyCollection(int arraySize){
+        numArray = new int[arraySize];
+    }
 
-	/**
-	 * You can declare local/global variables as per your requirement
-	 */
-	@SuppressWarnings("unused")
-	private String[] numArray;
-	
-	public MyCollection(int arraySize){
-		numArray = new String[arraySize];
-	}
+    public int search(int searchingNum) {
+        int position=-1;
+        for(int i=0;i<NumberOfElementsPresent();i++) {
+            if(numArray[i]==searchingNum) {
+                position = i;
+                break;
+            }
+        }
+        return position;
+    }
 
-	public int search(String searchingNum) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    public boolean add(int numberToAdd) {
+        
+        int size = NumberOfElementsPresent();
+    
+        if(size!=numArray.length&&search(numberToAdd)==-1) {
+            numArray[size]=numberToAdd;
+            return true;
+        }
+        else if(size==numArray.length) {
+            doubleCapacity();
+            size = NumberOfElementsPresent();
+            numArray[size]=numberToAdd;
+            return true;
+        }
+        
+        
+        return false;
+    }
 
-	public boolean add(String numberToAdd) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    public void doubleCapacity() {
+        int[] double_array = new int[numArray.length*2];
+        for(int i=0;i<numArray.length;i++) {
+            double_array[i] = numArray[i];
+        }
+        numArray = double_array;
+    }
 
-	public void doubleCapacity() {
-		// TODO Auto-generated method stub
+    public boolean remove(int numberToRemove) {
+        int position=search(numberToRemove);
+        if(position!=-1) {
+        for(int i=position;i<NumberOfElementsPresent()-1;i++) {
+            numArray[i]=numArray[i+1];
+        }
+        numArray[NumberOfElementsPresent()-1]=0;
+        return true;
+        }
+        return false;
+    }
 
-	}
+    public int getCount() {
+        return NumberOfElementsPresent();
+    }
 
-	public boolean remove(String numberToRemove) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public int getCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	public String[] rotate(int n) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public int[] rotate(int n) {
+        int[] dummy_array =new int[n];
+        
+        for(int i=0;i<n;i++) {
+            dummy_array[i]=numArray[i];
+        }
+        for(int i=n;i<NumberOfElementsPresent();i++) {
+            numArray[i-n]=numArray[i];
+        }
+        for(int i=0;i<n;i++) {
+            numArray[NumberOfElementsPresent()-n+i]=dummy_array[i];
+        }
+        
+        
+        return null;
+    }
+    
+    public String toString() {
+        String array="{";
+        for(int i=0;i<NumberOfElementsPresent();i++) {
+            array=array+Integer.toString(numArray[i])+",";
+        }
+        if(array.charAt(array.length()-1)==',') {
+            array=array.substring(0, array.length()-1);
+        }
+        array=array+"}";
+        return array;
+    }
+    
+    private int NumberOfElementsPresent() {
+        int count=0;
+        for(int i=0;i<numArray.length;i++) {
+            if(numArray[i]!=0)
+                count++;
+        }
+        return count;
+    }
+    
 
 }
